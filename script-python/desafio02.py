408616eccf7b46692c98c552a307e728caa4d6fb
Danilo Andrade <daniloams14@gmail.com>
2020-04-08 18:00:13 -0300

    commit plus archives
    tava aprendendo a usar isso aqui, é nois

---
+++ script-python/desafio02.py
@@ -0,0 +1,6 @@
+nome = input('Qual é o seu nome?')
+dia = input('Em qual dia você nasceu?')
+mês = input('No qual mês?')
+ano = input('Em qual lindissmo ano?')
+
+print('Oi,', nome, 'você nasceu no dia', dia, mês, ano, '. Correto?')
---
+++ script-python/desafio03.py
@@ -0,0 +1,4 @@
+numero1 = input('entre com o numero 1')
+numero2 = input('entre com o numero2')
+soma = (numero1 + numero2)
+print('A soma é:', soma)
